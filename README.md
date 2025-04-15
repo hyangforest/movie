527a0ed46fbf1fc6e0238f30043382a9f6b3d778
Hyangrim Lee <hyangrim.lee@kims.co.kr>
2025-04-15 15:18:52 +0900

    api 정리

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# 서비스
+# 배경
+- React + CRA 공부
+- 컴포넌트 재사용
+
+# 페이지
+1. 메인 리스트 
+    - 최신 영화
+    - 최신 드라마
+    - 제목 검색
+2. 상세 뷰
+
+# 외부 서비스
 [https://www.themoviedb.org](https://www.themoviedb.org)
 
 # npm
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,57 +1,23 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import MediaCard from "../components/MediaCard";
 import { Container, Grid, Header } from "./Home.styled";
-import { formatMediaData } from '../lib/media/formatMediaData';
-
-const API_KEY = process.env.REACT_APP_TMDB_API_KEY; // 환경변수 REACT_APP 접두어 필수
+import { fetchNowPlaying, fetchTV } from "../api/tmdb";
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
   const [tvShows, setTVShows] = useState([]);
 
-  // 최신 영화
-  const fetchNowPlaying = async () => {
-    try {
-      const res = await axios.get(
-        `https://api.themoviedb.org/3/movie/now_playing?`,
-        {
-          params: {
-            api_key: API_KEY,
-            language: "ko-KR",
-            page: 1,
-          },
-        }
-      );
-
-      // 8개
-      setMovies(res.data.results.slice(0, 8));
-    } catch (error) {
-      console.error("오류 발생", error);
-    }
-  };
-
-  // 최신 TV
-  const fetchTV = async () => {
-    try {
-      const res = await axios.get('https://api.themoviedb.org/3/discover/tv', {
-        params: {
-          api_key: API_KEY,
-          language: 'ko-KR',
-          sort_by: 'popularity.desc',
-          with_origin_country: 'KR',
-          first_air_date_year: new Date().getFullYear(), // 올해 첫 방영
-        },
-      });
-      setTVShows(res.data.results.slice(0, 8));
-    } catch (error) {
-      console.error("오류 발생", error);
-    }    
-  };
-
   useEffect(() => {
-    fetchNowPlaying();
+    const fetchData = async () => {
+      const movieDate = await fetchNowPlaying();
+      const tvData = await fetchTV();
+      
+      setMovies(movieDate);
+      setTVShows(tvData);
+    };
+    
     fetchTV();
+    fetchData();
   }, []);
 
   return (
@@ -59,13 +25,13 @@ export default function Home() {
       <Header>최신 영화</Header>
       <Grid>
         {movies.map((data) => (
-          <MediaCard key={data.id} data={formatMediaData(data, 'movie')} />
+          <MediaCard key={data.id} data={data} />
         ))}
       </Grid>
       <Header>최신 한국 드라마</Header>
       <Grid>
         {tvShows.map((data) => (
-          <MediaCard key={data.id} data={formatMediaData(data, 'tv')}/>
+          <MediaCard key={data.id} data={data}/>
         ))}
       </Grid>
       <div>&nbsp;</div>
